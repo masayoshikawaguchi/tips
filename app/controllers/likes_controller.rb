63bc6b8fd8693b0f25c0cfd569969eb7f5613953
Masayoshi Kawaguchi <k.liverpool0344@gmail.com>
2018-03-21 06:19:41 +0000

    count iine

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   def create
+    @micropost = Micropost.find(params[:micropost_id])
     @like = Like.create(user_id: current_user.id, micropost_id: params[:micropost_id])
     @likes = Like.where(micropost_id: params[:micropost_id])
   end
 
   def destroy
+    @micropost = Micropost.find(params[:micropost_id])
     like = Like.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
     like.destroy
     @likes = Like.where(micropost_id: params[:micropost_id])
